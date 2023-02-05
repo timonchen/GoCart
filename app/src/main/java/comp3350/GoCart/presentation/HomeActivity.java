51553e2e33b66a18cfacfbe2ea7eb7f736e10dda
Ryan Petrillo <petrillr@myumanitoba.ca>
2023-02-05 17:48:31 -0600

    Fixed compile and runtime issues and cleaned up classes

---
+++ app/src/main/java/comp3350/GoCart/presentation/HomeActivity.java
@@ -0,0 +1,20 @@
+package comp3350.GoCart.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import comp3350.GoCart.R;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
