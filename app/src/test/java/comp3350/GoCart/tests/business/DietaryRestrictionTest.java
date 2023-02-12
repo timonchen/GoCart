ad1ca9ffb6e947fd1a07ea100a2cd57366211df9
hridaimehta <mehtah1@myumanitoba.ca>
2023-02-12 10:48:07 -0600

    1.Added comments 2.Added ProductTest.java in object testing 3.Added StoreTest.java in object testing

--- app/src/test/java/comp3350/GoCart/tests/business/DietaryRestrictionTest.java
+++ app/src/test/java/comp3350/GoCart/tests/business/DietaryRestrictionTest.java
@@ -1,63 +1,59 @@
 package comp3350.GoCart.tests.business;
-
 import junit.framework.TestCase;
-
-import org.junit.Assert;
 import org.junit.Test;
 import comp3350.GoCart.business.AccessProducts;
 import comp3350.GoCart.objects.Product;
-import comp3350.GoCart.application.Services;
-import comp3350.GoCart.objects.Product;
-import comp3350.GoCart.persistence.ProductPersistence;
-import comp3350.GoCart.persistence.stubs.ProductPersistenceStub;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.ArrayList;
-
+import static org.junit.Assert.*;
 
 
 public class DietaryRestrictionTest extends TestCase {
     boolean isEmpty;
     private List<Product> list;
-    private AccessProducts se;
+    private AccessProducts accessProducts;
+
+    public DietaryRestrictionTest() {
+        super();
+        accessProducts = new AccessProducts();
+    }
 
     @Test
     public void testEmptyList() {
-        System.out.println("\nGet products: empty list");
+        List<Product> testList = accessProducts.getDietaryProducts();
 
+        assertFalse("Should not be empty", testList.isEmpty());
 
-        list = new ArrayList<>();
-        isEmpty = true;
-        assertTrue(isEmpty == (list.isEmpty()));
-
-        System.out.println("Finished testCalculateGPA: empty list");
+        System.out.println("Finished testEmptyList: empty list");
     }
 
     @Test
-    public void testDietaryRestriction(){
+    public void testDietaryRestriction() {
         System.out.println("\nchecking dietary restriction in a product");
-        Product p1 =new Product("Banana", new BigDecimal("1.05"),false);
-        Product p2 = (new Product("Rye Bread", new BigDecimal("1.45"),true));
-        assertFalse(p1.hasPeanutAllergy());
-        assertTrue(p2.hasPeanutAllergy());
+        Product p1 = new Product("Banana", new BigDecimal("1.05"), false);
+        Product p2 = (new Product("Rye Bread", new BigDecimal("1.45"), true));
+        assertFalse("The product should have peanut allergy", p1.hasPeanutAllergy());
+        assertTrue("The product should not have peanut allergy", p2.hasPeanutAllergy());
+        System.out.println("Finished testDietaryRestriction");
 
     }
 
     @Test
-    public void testShowDietaryRestrictedProducts(){
+    public void testShowDietaryProducts() {
         System.out.println("\nGet dietaryRestricted products");
         list = new ArrayList<>();
-        list.add(new Product("Banana", new BigDecimal("1.05"),false));
-        list.add(new Product("Beef Jerkey", new BigDecimal("9.99"),false));
-        list.add(new Product("toilet paper", new BigDecimal("15.99"),false));
+        list.add(new Product("Banana", new BigDecimal("1.05"), false));
+        list.add(new Product("Beef Jerkey", new BigDecimal("9.99"), false));
+        list.add(new Product("toilet paper", new BigDecimal("15.99"), false));
+        list.add(new Product("Ice-cream", new BigDecimal("5.99"), false));
 
-        ProductPersistenceStub test = new ProductPersistenceStub();
-        List<Product> testList=test.getDietaryRestrictedProducts();
-        System.out.println(testList.toString());
-        assertTrue( testList.containsAll(list)&&(list).containsAll(testList));
+        List<Product> testList = accessProducts.getDietaryProducts();
+        assertTrue(testList.containsAll(list) && (list).containsAll(testList));
+
+        System.out.println("Finished testShowDietaryProducts");
 
     }
-    
+
 
 }
