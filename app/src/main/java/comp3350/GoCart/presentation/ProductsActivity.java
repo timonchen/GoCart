a45442d0e09d7f077a93122d65e3e6a1c7a552a1
benedict <agupitab@myumanitoba.ca>
2023-03-02 12:02:10 -0600

    Products Page UI is filled with stub items. (Name, address, image).

--- app/src/main/java/comp3350/GoCart/presentation/ProductsActivity.java
+++ app/src/main/java/comp3350/GoCart/presentation/ProductsActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import java.util.List;
+
+import comp3350.GoCart.R;
+import comp3350.GoCart.objects.Product;
 import comp3350.GoCart.objects.Store;
 
 public class ProductsActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
 
         Store store = getIntent().getParcelableExtra("selected_store"); // Store products are from
+
+        List<Product> storeProducts = store.getStoreProducts();
     }
 }
