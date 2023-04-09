a5b6c72bfff7e1d2164b0db4f5034633bee8ad38
hridaimehta <mehtah1@myumanitoba.ca>
2023-04-09 13:50:37 -0500

    removed exception, was not being used anywhere

--- app/src/main/java/comp3350/GoCart/exceptions/PersistenceException.java
+++
@@ -1,8 +0,0 @@
-package comp3350.GoCart.exceptions;
-
-public class PersistenceException extends Exception{
-    public PersistenceException(final Exception cause) {
-        super(cause);
-    }
-
-}
